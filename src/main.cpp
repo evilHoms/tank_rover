c85265e64cfc703e84c2fef6155989610be673a7
evilHoms <homspage@gmail.com>
2022-01-27 18:11:22 +0300

    Added nrf libs and base code to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,44 @@
 #include <Arduino.h>
+#include "RF24Service.h"
+
+// Remove, when built for production
+#define SHOW_DEBUG
+
+RF24Service radio(10, 9);
+byte request[8];
 
 void setup() {
   Serial.begin(9600);
 
-  // #init NRF24
+  // #begin Init nrf24 as resiever ----------------------------/
+  #ifdef SHOW_DEBUG
+  radio.showDebug();
+  #endif
+  radio.init(RF24_PA_MAX, RF24_1MBPS, sizeof(request));
+  radio.asReciever();
+  // #end Init nrf24 as resiever ------------------------------/
 
-  // #init Motors
+  // #todo init Motors
 }
 
 void loop() {
-  // #get data from nrf
+  // #begin Get data from nrf24 -------------------------------/
+  byte pipeId = 255;
+  while (radio.available(&pipeId)) {
+    byte payloadSize = radio.getPayloadSize();
+    radio.read(&request, payloadSize);
+  }
+  bool isNoData = pipeId == 255;
+  // #end Get data from nrf24 ---------------------------------/
 
-  // #if no data got from nrf, then skip other steps
+  if (!isNoData) {
+    // #todo map nrf data
 
-  // #map nrf data
+    // #todo pass mapped data to motors
 
-  // #pass mapped data to motors
+    // #todo_optional get battery info via currency devider
+    // #rodo_optional send battery info back to controller
+  }
 
-  // #optional get battery info via currency devider
-  // #optional send battery info back to controller
-
-  Serial.println(micros());
   delay(1000);
 }
